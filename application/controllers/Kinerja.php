2fe70ab4e55c2deeaeabf59b8f1c5bac73f0be3c
RyanB-Laptop <ryan.brilliant.nirwana@gmail.com>
2021-04-22 13:26:05 +0700

    fix insert perencanaan

--- application/controllers/Kinerja.php
+++ application/controllers/Kinerja.php
@@ -1,37 +1,40 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Kinerja extends CI_Controller {
+class Kinerja extends CI_Controller
+{
 
 	function __construct()
-    {
-        parent::__construct();
-        $this->load->library('parser');
-        $this->load->library('session');
+	{
+		parent::__construct();
+		$this->load->library('parser');
+		$this->load->library('session');
 		$this->load->model('Kinerja_model');
-		
-		
-		if($this->session->userdata('status') != "login"){
+
+
+		if ($this->session->userdata('status') != "login") {
 			redirect('login/login');
 		}
-    }
-	
-	public function index(){	
-		$unit = $_SESSION['id_unit'];
-		$data['sub_unit']= $this->Kinerja_model->get_sub_unit($unit);
-		$data['content'] = $this->load->view('Kinerja_vw', $data ,true);
-        $this->parser->parse('template/backend', $data);
 	}
-	
-	function get_data_content(){
+
+	public function index()
+	{
+		$unit = $_SESSION['id_unit'];
+		$data['sub_unit'] = $this->Kinerja_model->get_sub_unit($unit);
+		$data['content'] = $this->load->view('Kinerja_vw', $data, true);
+		$this->parser->parse('template/backend', $data);
+	}
+
+	function get_data_content()
+	{
 		$unit = $_POST['unit'];
-		$url = 'http://negaraapi.madiunkota.go.id:8888/v_renstra/109/2019/2024/'.$unit.'';
+		$url = 'http://negaraapi.madiunkota.go.id:8888/v_renstra/109/2019/2024/' . $unit . '';
 		$json = file_get_contents($url);
 		$data_json = json_decode($json, TRUE);
 		$data = $data_json['data'];
-		
-		$output='';
-		$output.='<table id="tb_prog" class="table table-bordered table-striped">
+
+		$output = '';
+		$output .= '<table id="tb_prog" class="table table-bordered table-striped">
 					<thead bgcolor="#5d80a3">
 						<tr style="color: #fff;">
 						  <th>No</th>
@@ -47,185 +50,185 @@ class Kinerja extends CI_Controller {
 					</thead>
 					<tbody>';
 		//$arr = array();
-			if(!empty($data)){
-				$x=1;
-				foreach($data as $k=>$v){
-					$output.='
+		if (!empty($data)) {
+			$x = 1;
+			foreach ($data as $k => $v) {
+				$output .= '
 						<tr style="color: #fff;">
-							<td rowspan="4" bgcolor="#3793de">'.$x.'</td>
-							<td bgcolor="#3793de">'.$v['kode_bidang_urusan'].'</td>
-							<td bgcolor="#3793de" colspan="4">'.$v['nama_bidang_urusan'].'</td>
+							<td rowspan="4" bgcolor="#3793de">' . $x . '</td>
+							<td bgcolor="#3793de">' . $v['kode_bidang_urusan'] . '</td>
+							<td bgcolor="#3793de" colspan="4">' . $v['nama_bidang_urusan'] . '</td>
 						</tr>
 						<tr style="color: #fff;">
-							<td bgcolor="#3793de">'. $v['kode_program'].'</td>
-							<td bgcolor="#3793de" colspan="4">'.$v['nama_program'].'</td>
+							<td bgcolor="#3793de">' . $v['kode_program'] . '</td>
+							<td bgcolor="#3793de" colspan="4">' . $v['nama_program'] . '</td>
 						</tr>
 						<tr>
-							<td>'.$v['kode_giat'].'</td>
-							<td>'.$v['nama_giat'].'</td>
-							<td colspan="3">'.number_format($v['pagu_2'], 0, ",", ".").'</td>
+							<td>' . $v['kode_giat'] . '</td>
+							<td>' . $v['nama_giat'] . '</td>
+							<td colspan="3">' . number_format($v['pagu_2'], 0, ",", ".") . '</td>
 						</tr>
 						<tr>
-							<td>'.$v['kode_sub_giat'].'</td>
-							<td>'.$v['nama_sub_giat'].'</td>
-							<td>'.number_format($v['pagu_sub_2'], 0, ",", ".").'</td>
+							<td>' . $v['kode_sub_giat'] . '</td>
+							<td>' . $v['nama_sub_giat'] . '</td>
+							<td>' . number_format($v['pagu_sub_2'], 0, ",", ".") . '</td>
 							<td align="center">
-								<a href="kinerja/perencanaan/'.$v['kode_sub_giat'].'"><button type="button" class="btn btn-success">Perencanaan</button></a>
+								<a href="kinerja/perencanaan/' . $v['kode_sub_giat'] . '"><button type="button" class="btn btn-success">Perencanaan</button></a>
 								<a href="#"><button type="button" class="btn btn-info">Realisasi</button></a>
 							</td>
 							<td align="center"><button type="button" class="btn btn-danger"><i class="fa fa-unlock"></i></button></td>
 						</tr>
 						';
-					$x++;
-				}
-			}else{
-				$output.='<tr>
+				$x++;
+			}
+		} else {
+			$output .= '<tr>
 							<td colspan="6" align="center">no data available!</td>
 						</tr>';
-			}							
-		$output.='</tbody>
+		}
+		$output .= '</tbody>
 				</table>';
 		echo $output;
-		
 	}
-	
-	function perencanaan($id){
+
+	function perencanaan($id)
+	{
 		$kode_kegiatan = $this->uri->segment('3');;
 		$data['bulan'] = $this->Kinerja_model->get_bulan();
-		$data_view['content']= $this->load->view('perencanaan_vw', $data, true);
-        $this->parser->parse('template/backend', $data_view);
-
+		$data_view['content'] = $this->load->view('perencanaan_vw', $data, true);
+		$this->parser->parse('template/backend', $data_view);
 	}
-	
-	function simpan_perencanaan(){
-		$kode_perencanaan = $_POST['kode_perencanaan'];
-		$indikator = $_POST['indikator'];
-		$kalkulasi = $_POST['kalkulasi'];
+
+	function simpan_perencanaan()
+	{
+		$kode_perencanaan = $this->input->post('kode_perencanaan');
+		$indikator = $this->input->post('indikator');
+		$kalkulasi = $this->input->post('kalkulasi');
 		//januari
-		$jan_w1 = $_POST['jan_w1'];
-		$jan_w2 = $_POST['jan_w2'];
-		$jan_w3 = $_POST['jan_w3'];
-		$jan_w4 = $_POST['jan_w4'];
+		$jan_w1 = $this->input->post('jan_w1');
+		$jan_w2 = $this->input->post('jan_w2');
+		$jan_w3 = $this->input->post('jan_w3');
+		$jan_w4 = $this->input->post('jan_w4');
 		//februari
-		$feb_w1 = $_POST['feb_w1'];
-		$feb_w2 = $_POST['feb_w2'];
-		$feb_w3 = $_POST['feb_w3'];
-		$feb_w4 = $_POST['feb_w4'];
+		$feb_w1 = $this->input->post('feb_w1');
+		$feb_w2 = $this->input->post('feb_w2');
+		$feb_w3 = $this->input->post('feb_w3');
+		$feb_w4 = $this->input->post('feb_w4');
 		//maret
-		$mar_w1 = $_POST['mar_w1'];
-		$mar_w2 = $_POST['mar_w2'];
-		$mar_w3 = $_POST['mar_w3'];
-		$mar_w4 = $_POST['mar_w4'];
+		$mar_w1 = $this->input->post('mar_w1');
+		$mar_w2 = $this->input->post('mar_w2');
+		$mar_w3 = $this->input->post('mar_w3');
+		$mar_w4 = $this->input->post('mar_w4');
 		//april
-		$apr_w1 = $_POST['apr_w1'];
-		$apr_w2 = $_POST['apr_w2'];
-		$apr_w3 = $_POST['apr_w3'];
-		$apr_w4 = $_POST['apr_w4'];
+		$apr_w1 = $this->input->post('apr_w1');
+		$apr_w2 = $this->input->post('apr_w2');
+		$apr_w3 = $this->input->post('apr_w3');
+		$apr_w4 = $this->input->post('apr_w4');
 		//mei
-		$mei_w1 = $_POST['mei_w1'];
-		$mei_w2 = $_POST['mei_w2'];
-		$mei_w3 = $_POST['mei_w3'];
-		$mei_w4 = $_POST['mei_w4'];
+		$mei_w1 = $this->input->post('mei_w1');
+		$mei_w2 = $this->input->post('mei_w2');
+		$mei_w3 = $this->input->post('mei_w3');
+		$mei_w4 = $this->input->post('mei_w4');
 		//juni
-		$jun_w1 = $_POST['jun_w1'];
-		$jun_w2 = $_POST['jun_w2'];
-		$jun_w3 = $_POST['jun_w3'];
-		$jun_w4 = $_POST['jun_w4'];
+		$jun_w1 = $this->input->post('jun_w1');
+		$jun_w2 = $this->input->post('jun_w2');
+		$jun_w3 = $this->input->post('jun_w3');
+		$jun_w4 = $this->input->post('jun_w4');
 		//juli
-		$jul_w1 = $_POST['jul_w1'];
-		$jul_w2 = $_POST['jul_w2'];
-		$jul_w3 = $_POST['jul_w3'];
-		$jul_w4 = $_POST['jul_w4'];
+		$jul_w1 = $this->input->post('jul_w1');
+		$jul_w2 = $this->input->post('jul_w2');
+		$jul_w3 = $this->input->post('jul_w3');
+		$jul_w4 = $this->input->post('jul_w4');
 		//agustus
-		$agt_w1 = $_POST['agt_w1'];
-		$agt_w2 = $_POST['agt_w2'];
-		$agt_w3 = $_POST['agt_w3'];
-		$agt_w4 = $_POST['agt_w4'];
+		$agt_w1 = $this->input->post('agt_w1');
+		$agt_w2 = $this->input->post('agt_w2');
+		$agt_w3 = $this->input->post('agt_w3');
+		$agt_w4 = $this->input->post('agt_w4');
 		//september
-		$sep_w1 = $_POST['sep_w1'];
-		$sep_w2 = $_POST['sep_w2'];
-		$sep_w3 = $_POST['sep_w3'];
-		$sep_w4 = $_POST['sep_w4'];
+		$sep_w1 = $this->input->post('sep_w1');
+		$sep_w2 = $this->input->post('sep_w2');
+		$sep_w3 = $this->input->post('sep_w3');
+		$sep_w4 = $this->input->post('sep_w4');
 		//oktober
-		$okt_w1 = $_POST['okt_w1'];
-		$okt_w2 = $_POST['okt_w2'];
-		$okt_w3 = $_POST['okt_w3'];
-		$okt_w4 = $_POST['okt_w4'];
+		$okt_w1 = $this->input->post('okt_w1');
+		$okt_w2 = $this->input->post('okt_w2');
+		$okt_w3 = $this->input->post('okt_w3');
+		$okt_w4 = $this->input->post('okt_w4');
 		//november
-		$nov_w1 = $_POST['nov_w1'];
-		$nov_w2 = $_POST['nov_w2'];
-		$nov_w3 = $_POST['nov_w3'];
-		$nov_w4 = $_POST['nov_w4'];
+		$nov_w1 = $this->input->post('nov_w1');
+		$nov_w2 = $this->input->post('nov_w2');
+		$nov_w3 = $this->input->post('nov_w3');
+		$nov_w4 = $this->input->post('nov_w4');
 		//desember
-		$des_w1 = $_POST['des_w1'];
-		$des_w2 = $_POST['des_w2'];
-		$des_w3 = $_POST['des_w3'];
-		$des_w4 = $_POST['des_w4'];
+		$des_w1 = $this->input->post('des_w1');
+		$des_w2 = $this->input->post('des_w2');
+		$des_w3 = $this->input->post('des_w3');
+		$des_w4 = $this->input->post('des_w4');
 
 		$data = array(
-			"kode_perencanaan"=>$kode_perencanaan,
-			"nama_aktivitas"=> $indikator,
-			"total_kalkulasi"=> $kalkulasi,
-			"jan_w1"=> $jan_w1,
-			"jan_w2"=> $jan_w2,
-			"jan_w3"=> $jan_w3,
-			"jan_w4"=> $jan_w4,
-			"feb_w1"=> $feb_w1,
-			"feb_w2"=> $feb_w2,
-			"feb_w3"=> $feb_w3,
-			"feb_w4"=> $feb_w4,
-			"mar_w1"=> $mar_w1,
-			"mar_w2"=> $mar_w2,
-			"mar_w3"=> $mar_w3,
-			"mar_w4"=> $mar_w4,
-			"apr_w1"=> $apr_w1,
-			"apr_w2"=> $apr_w2,
-			"apr_w3"=> $apr_w3,
-			"apr_w4"=> $apr_w4,
-			"mei_w1"=> $mei_w1,
-			"mei_w2"=> $mei_w2,
-			"mei_w3"=> $mei_w3,
-			"mei_w4"=> $mei_w4,
-			"jun_w1"=> $jun_w1,
-			"jun_w2"=> $jun_w2,
-			"jun_w3"=> $jun_w3,
-			"jun_w4"=> $jun_w4,
-			"jul_w1"=> $jul_w1,
-			"jul_w2"=> $jul_w2,
-			"jul_w3"=> $jul_w3,
-			"jul_w4"=> $jul_w4,
-			"agt_w1"=> $agt_w1,
-			"agt_w2"=> $agt_w2,
-			"agt_w3"=> $agt_w3,
-			"agt_w4"=> $agt_w4,
-			"sep_w1"=> $sep_w1,
-			"sep_w2"=> $sep_w2,
-			"sep_w3"=> $sep_w3,
-			"sep_w4"=> $sep_w4,
-			"okt_w1"=> $okt_w1,
-			"okt_w2"=> $okt_w2,
-			"okt_w3"=> $okt_w3,
-			"okt_w4"=> $okt_w4,
-			"nov_w1"=> $nov_w1,
-			"nov_w2"=> $nov_w2,
-			"nov_w3"=> $nov_w3,
-			"nov_w4"=> $nov_w4,
-			"des_w1"=> $des_w1,
-			"des_w2"=> $des_w2,
-			"des_w3"=> $des_w3,
-			"des_w4"=> $des_w4			
+			"kode_perencanaan" => $kode_perencanaan,
+			"nama_aktifitas" => $indikator,
+			"total_kalkulasi" => $kalkulasi,
+			"jan_w1" => $jan_w1,
+			"jan_w2" => $jan_w2,
+			"jan_w3" => $jan_w3,
+			"jan_w4" => $jan_w4,
+			"feb_w1" => $feb_w1,
+			"feb_w2" => $feb_w2,
+			"feb_w3" => $feb_w3,
+			"feb_w4" => $feb_w4,
+			"mar_w1" => $mar_w1,
+			"mar_w2" => $mar_w2,
+			"mar_w3" => $mar_w3,
+			"mar_w4" => $mar_w4,
+			"apr_w1" => $apr_w1,
+			"apr_w2" => $apr_w2,
+			"apr_w3" => $apr_w3,
+			"apr_w4" => $apr_w4,
+			"mei_w1" => $mei_w1,
+			"mei_w2" => $mei_w2,
+			"mei_w3" => $mei_w3,
+			"mei_w4" => $mei_w4,
+			"jun_w1" => $jun_w1,
+			"jun_w2" => $jun_w2,
+			"jun_w3" => $jun_w3,
+			"jun_w4" => $jun_w4,
+			"jul_w1" => $jul_w1,
+			"jul_w2" => $jul_w2,
+			"jul_w3" => $jul_w3,
+			"jul_w4" => $jul_w4,
+			"agt_w1" => $agt_w1,
+			"agt_w2" => $agt_w2,
+			"agt_w3" => $agt_w3,
+			"agt_w4" => $agt_w4,
+			"sep_w1" => $sep_w1,
+			"sep_w2" => $sep_w2,
+			"sep_w3" => $sep_w3,
+			"sep_w4" => $sep_w4,
+			"okt_w1" => $okt_w1,
+			"okt_w2" => $okt_w2,
+			"okt_w3" => $okt_w3,
+			"okt_w4" => $okt_w4,
+			"nov_w1" => $nov_w1,
+			"nov_w2" => $nov_w2,
+			"nov_w3" => $nov_w3,
+			"nov_w4" => $nov_w4,
+			"des_w1" => $des_w1,
+			"des_w2" => $des_w2,
+			"des_w3" => $des_w3,
+			"des_w4" => $des_w4
 		);
+
 		//print_r($data);die;
-		$data_input = $this->Kinerja_model->input_data('tb_perencanaan',$data);
-		
-		if($data_input>0){
-				echo "Sukses menyimpan data";
-				redirect('kinerja');
+		$data_input = $this->Kinerja_model->input_data("tb_perencanaan", $data);
+
+		if ($data_input > 0) {
+			echo "Sukses menyimpan data";
+			redirect('kinerja');
+		} else {
+			echo "Gagal menyimpan data";
+			var_dump($data);
+			redirect('kinerja');
 		}
-		else{
-				echo "Gagal menyimpan data";
-				redirect('kinerja');
-		}	
 	}
-		
 }
--- application/models/Kinerja_model.php
+++ application/models/Kinerja_model.php
@@ -1,63 +1,72 @@
-<?php 
+<?php
 
-class Kinerja_model extends CI_Model {
-	
-	function get_sub_unit($unit){
+class Kinerja_model extends CI_Model
+{
+
+	function get_sub_unit($unit)
+	{
 		$this->db->select('*');
-		$this->db->from('tbl_sub_unit');		
-		$this->db->where('id_sub_unit',$unit);			
+		$this->db->from('tbl_sub_unit');
+		$this->db->where('id_sub_unit', $unit);
 		$query = $this->db->get();
 		return $query->result_array();
 	}
-	
-	function get_bulan(){
+
+	function get_bulan()
+	{
 		$this->db->select('*');
-		$this->db->from('tb_bulan');		
+		$this->db->from('tb_bulan');
 		$query = $this->db->get();
 		return $query->result_array();
 	}
-	
-	function val_bidang($unit){
+
+	function val_bidang($unit)
+	{
 		$query = $this->db->query('select a.kode_unit, b.kode_bidang, b.nama_bidang from tb_unit a, tb_bidang b 
 									where a.kode_bidang = b.kode_bidang
-									and a.kode_unit="'.$unit.'"');
-		return $query->result_array();		
+									and a.kode_unit="' . $unit . '"');
+		return $query->result_array();
 	}
-	
-	function get_data($bidang,$unit){
+
+	function get_data($bidang, $unit)
+	{
 		$query = $this->db->query('SELECT 
-							(SELECT COUNT(kode_program) FROM tb_program WHERE kode_bidang = "'.$bidang.'") as jml_program, 
+							(SELECT COUNT(kode_program) FROM tb_program WHERE kode_bidang = "' . $bidang . '") as jml_program, 
 							(SELECT COUNT(kode_kegiatan) FROM tb_kegiatan join tb_program on tb_kegiatan.kode_program = tb_program.kode_program
-							join tb_bidang on tb_program.kode_bidang = tb_bidang.kode_bidang WHERE kode_bidang = "'.$bidang.'") as jml_kegiatan, 
-							nama_unit from tb_unit where kode_unit="'.$unit.'"');
-		return $query->result_array();		
+							join tb_bidang on tb_program.kode_bidang = tb_bidang.kode_bidang WHERE kode_bidang = "' . $bidang . '") as jml_kegiatan, 
+							nama_unit from tb_unit where kode_unit="' . $unit . '"');
+		return $query->result_array();
 	}
-	
-	
-	function get_data_table($unit){
+
+
+	function get_data_table($unit)
+	{
 		$query = $this->db->query('select a.kode_program, a.nama_program, b.kode_kegiatan, b.nama_kegiatan, c.kode_sub_kegiatan, c.nama_sub_kegiatan, d.kode_unit 
 									from tb_program a, tb_kegiatan b, tb_sub_kegiatan c, tb_unit d where a.kode_program = b.kode_program 
-									and b.kode_kegiatan = c.kode_kegiatan and a.kode_bidang = d.kode_bidang and d.kode_unit = "'.$unit.'"');
-		return $query->result_array();		
+									and b.kode_kegiatan = c.kode_kegiatan and a.kode_bidang = d.kode_bidang and d.kode_unit = "' . $unit . '"');
+		return $query->result_array();
 	}
-	
-	function get_info($kode_kegiatan){
+
+	function get_info($kode_kegiatan)
+	{
 		$query = $this->db->query('select a.nama_kegiatan, b.nama_indikator_keg, format(c.pagu_perubahan,0) as pagu_perubahan
 									from tb_kegiatan a, tb_indikator_kegiatan b, tb_ev_tgt_keg c 
 									where a.kode_kegiatan=b.kode_kegiatan 
 									and b.kode_kegiatan= c.kode_kegiatan 
-									and a.kode_kegiatan="'.$kode_kegiatan.'"');
-		return $query->result_array();		
+									and a.kode_kegiatan="' . $kode_kegiatan . '"');
+		return $query->result_array();
 	}
-	
-	
-	function get_indikator($kode_kegiatan){
-		$query = $this->db->query('select nama_indikator_keg, satuan from tb_indikator_kegiatan where kode_kegiatan="'.$kode_kegiatan.'"');
-		return $query->result_array();		
+
+
+	function get_indikator($kode_kegiatan)
+	{
+		$query = $this->db->query('select nama_indikator_keg, satuan from tb_indikator_kegiatan where kode_kegiatan="' . $kode_kegiatan . '"');
+		return $query->result_array();
 	}
-	
-	function input_data($table,$data){
-		$this->db->insert($table,$data);
-        return $this->db->insert_id();
+
+	function input_data($table, $data)
+	{
+		$this->db->insert($table, $data);
+		return $this->db->insert_id();
 	}
 }
